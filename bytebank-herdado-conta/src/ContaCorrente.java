b53cf1141ce9757ef692191a79f9e8fca199c96d
Marlon Jhoni Lima Antonelo <marlon.jhoni@hotmail.com>
2023-07-29 19:52:57 -0300

    atualização polimorfismo intercafe, implements

--- bytebank-herdado-conta/src/ContaCorrente.java
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -1,5 +1,5 @@
 
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 	
 	public ContaCorrente(int agencia, int numero) {
 		super(agencia, numero);
@@ -18,6 +18,13 @@ public class ContaCorrente extends Conta {
 		super.saldo += valor;
 		
 		
+	}
+
+
+	@Override
+	public double getValorImposto() {
+
+		return super.saldo * 0.01;
 	}
 	
 }
---
+++ bytebank-herdado-conta/src/TesteTributaveis.java
@@ -0,0 +1,15 @@
+
+public class TesteTributaveis {
+	public static void main(String[] args) {
+		ContaCorrente cc = new ContaCorrente(222, 222);
+		cc.deposita(100.0);
+		
+		SeguroDeVida seguro = new SeguroDeVida();
+		
+		CalculadorDeImposto cal = new CalculadorDeImposto()	;
+		cal.registra(cc);
+		cal.registra(seguro);
+		
+		System.out.println(cal.getTotalImposto());
+	}
+}
