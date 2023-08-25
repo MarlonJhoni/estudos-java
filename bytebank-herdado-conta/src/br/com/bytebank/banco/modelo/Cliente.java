27c4eccf2aae79b344201bfd883786e510495dad
Marlon Jhoni <marlon.jhoni@hotmail.com>
2023-08-25 09:32:36 -0300

    javadoc

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Cliente.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Cliente.java
@@ -1,6 +1,12 @@
 package br.com.bytebank.banco.modelo;
 
-
+/**
+ * 
+ * Classe que representa um cliente no Bytebank
+ * 
+ * @author Marlon Jhoni
+ * @version 0.1
+ */
 public class Cliente {
 	
 	private String nome;
--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -1,6 +1,11 @@
 package br.com.bytebank.banco.modelo;
 
-
+/**
+ * 
+ * Classe que representa uma moldura de uma conta
+ * 
+ * @author Marlon Jhoni
+ */
 public abstract class Conta {
 	
 	protected double saldo;
@@ -9,7 +14,11 @@ public abstract class Conta {
 	private Cliente  titular;
 	private static int total = 0;
 	
-	
+	/**
+	 * Construtor para inicializar o objeto Conta a partir da agencia e numero
+	 * @param agencia
+	 * @param numero
+	 */
 	public Conta(int agencia, int numero) {
 		Conta.total++;
 //		System.out.println("o total de contas e " + Conta.total);
@@ -23,6 +32,11 @@ public abstract class Conta {
 	//metodo
 	public abstract void deposita(double valor);
 	
+	/**
+	 * Valor precisa ser maior do que o saldo
+	 * @param valor
+	 * @throws SaldoInsuficienteException
+	 */
 	public void saca(double valor) throws SaldoInsuficienteException {
 		
 		if(this.saldo < valor) {
